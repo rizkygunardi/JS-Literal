3ba0e252b9ad1c1edd64f41427dd403917ebce29
Rizky Gunardi <rgunardi97@gmail.com>
2022-12-27 21:05:36 +0700

    Add files via upload
    update

---
+++ variabel.js
@@ -0,0 +1,24 @@
+// const name = "Rizky";
+// name = "Eduwork";
+// console.log(name);
+
+// const saya = {
+//   nama: "Rizky",
+//   umur: 25,
+// }
+
+// saya.kelas = "Quality Assurance";
+// console.log(saya);
+
+// const hobi = ["bersepeda", "ngoding", "bermain game"];
+// hobi.push("berenang");
+// console.log(hobi);
+
+//TUGAS
+let x = "10" + 10 + 10;
+let y = 10 + 10 + "10";
+console.log(x + y); //Hasilnya 1010102010
+
+//semua variabel secara normal akan dijumlahkan karena memiliki type data integer
+//namun jika ada type data string maka nilai integer secara otomatis akan dijadikan string
+//sehingga nilainya tidak dijumlahkan namun digabungkan
